5ef5e69f9dfed4565d305bfea86b58b9bbf92d5d
Riza Khan <rizakhan@Rizas-Mac-mini.local>
2022-08-01 16:55:51 -0400

    Footer component

--- src/molecules/AVFooter.vue
+++ src/molecules/AVFooter.vue
@@ -1,3 +1,32 @@
 <template>
-    <div>Footer component goes here</div>
+    <AVRow flex>
+        <a :href="props.privacyPolicy.link" target="_blank" class="text-grey">{{
+            props.privacyPolicy.text
+        }}</a>
+        <a
+            :href="props.termsOfService.link"
+            target="_blank"
+            class="text-grey"
+            >{{ props.termsOfService.text }}</a
+        >
+        <p>&copy; {{ year }} Avvinue, Inc</p>
+    </AVRow>
 </template>
+
+<script setup>
+import { ref, defineProps } from 'vue'
+import AVRow from '@/atoms/AVRow.vue'
+
+const props = defineProps({
+    privacyPolicy: {
+        type: Object,
+        default: () => ({ link: '#', text: 'Privacy Policy' }),
+    },
+    termsOfService: {
+        type: Object,
+        default: () => ({ link: '#', text: 'Terms of Service' }),
+    },
+})
+
+const year = ref(new Date().getFullYear())
+</script>
---
+++ src/views/AVFooter.vue
@@ -0,0 +1,7 @@
+<template>
+    <AVFooter> </AVFooter>
+</template>
+
+<script setup>
+import AVFooter from '@/molecules/AVFooter.vue'
+</script>
