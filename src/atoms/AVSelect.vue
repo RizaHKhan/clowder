05a00e00a1052f123f315752c973bdc5b516f86d
Riza Khan <rizakhan@Rizas-Mac-mini.local>
2022-08-01 16:05:28 -0400

    Showing av select

--- src/atoms/AVSelect.vue
+++ src/atoms/AVSelect.vue
@@ -1,5 +1,5 @@
 <template>
-    <q-select>
+    <q-select outlined>
         <template v-for="(_, slot) in $slots" #[slot]="scope">
             <slot :name="slot" v-bind="scope || {}" />
         </template>
---
+++ src/views/AVSelect.vue
@@ -0,0 +1,13 @@
+<template>
+    <AVSelect label="Select" :options="options" v-model="select" />
+    <p>{{ select }}</p>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import AVSelect from '@/atoms/AVSelect.vue'
+
+const select = ref('')
+
+const options = ['One', 'Two', 'Three']
+</script>
